a9149410da1bc7081f8ef6ab11e2f361f2b9821f
kengoDev <kengo.startup.dev@gmail.com>
2018-11-27 07:59:44 -0300

    some

--- video.py
+++ video.py
@@ -26,6 +26,7 @@ while video_capture.isOpened():
 
     # Grab a single frame of video
     ret, frame = video_capture.read()
+    frame = imutils.resize(frame, width=600)
     
     frame_count += 1
     if(frame_count%24 == 0):
